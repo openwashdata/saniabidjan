6a43cfbd89fa3a4b087e3771146e6aceb113581a
Mian Zhong <mzhong@ethz.ch>
2024-07-10 21:24:17 +0200

    add data-processing, update description, add draft dictionary

---
+++ R/saniabidjan.R
@@ -0,0 +1,33 @@
+#' saniabidjan: Title goes here
+#' 
+#' Description of the data goes here...
+#' 
+#' @format A tibble with 207 rows and 25 variables
+#' \describe{
+#'   \item{area_name}{Two areas of Abidjan, options including [1] Yopougon and [2] Abobo.}
+#'   \item{sex}{Sex of the respondent, options including [1] male and [2] female.}
+#'   \item{education}{Educational level of the respondent, options including [1] no, [2] coranic: Koranic school, [3] primary: primary school, [4] high school, and [5] graduate.}
+#'   \item{housing_type}{Types of housing, options including [1] individual and [2] common courtyard.}
+#'   \item{status}{Status of the respondent to the housing, options including [1] free host, [2] owner, and [3] renter.}
+#'   \item{lat_facilities}{to be available}
+#'   \item{facilities_loc}{Location of the latrine, options including [1] house, [2] yard, and [3] out of yard.}
+#'   \item{facilities_type}{Type of the latrine facility, options including [1] pit latrine, [2] improved latrine, and [3] public latrine.}
+#'   \item{lat_sharing}{Is the latrine shared?}
+#'   \item{nb_user}{Number of users.}
+#'   \item{impacts_of_sharing}{The impacts of sharing latrine, options including [1] no problems, [2] hygiene problems, [3] vulnerable to disease, and [4] waiting line.}
+#'   \item{lat_care}{Who takes care of the latrine cleaning, options including [1] no care, [2] myself, [3] private person, [4] round turn.}
+#'   \item{clean_freq_wk}{Weekly frequency of cleaning latrine, 2: 1-3 times, 4: 4 or more times.}
+#'   \item{tank_presence}{to be available}
+#'   \item{tank_location}{Location of the tank, options including [1] court yard, [2] not know, and [3] out of court yard.}
+#'   \item{latrines_nb}{Number of the latrines.}
+#'   \item{tk_status}{Does the tank perform well?}
+#'   \item{empty_freq_yr}{to be available}
+#'   \item{empty_mode}{Mode of emptying, options including [1] emptying truck, [2] manual emptying, and [3] truck and manual.}
+#'   \item{empty_price}{Price of emptying}
+#'   \item{wastwat_dispos}{options including [1] courtyard, [2] open drain, and [3] street.}
+#'   \item{waste_cont}{to be available}
+#'   \item{waste_cont_loc}{to be available options including [1] courtyard and [2] out of courtyard.}
+#'   \item{cont_empty_wk}{to be available}
+#'   \item{common_ill}{Common illness, options including [1] diarrhea, [2] infections, [3] malaria, and [4] typhoid fever.}
+#' }
+"saniabidjan"
--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -9,14 +9,67 @@ library(here)
 library(readr)
 library(readxl)
 library(openxlsx)
+library(dplyr)
 
 # Read data --------------------------------------------------------------------
-# data_in <- readr::read_csv("data-raw/dataset.csv")
-# codebook <- readxl::read_excel("data-raw/codebook.xlsx") |>
-#  clean_names()
-
+data_in <- readxl::read_excel("data-raw/Survey_base.xlsx")
 # Tidy data --------------------------------------------------------------------
-## Clean the raw data into a tidy format here
+## Modify variable types
+data_in <- data_in[,-c(1,4,6)]
+
+saniabidjan <- data_in |>
+  dplyr::mutate(dplyr::across(tidyselect::everything(), ~ dplyr::na_if(., "NA"))) |>
+  dplyr::mutate(dplyr::across(c(area_name, sex, education, housing_type, status, location,
+                         latrine_type, using_latrines, latrine_cleaning,
+                         tank_location, household_wastewater, common_diseases,
+                         impacts_of_sharing, tank_operation, emptying_frqce_yr, emptying_mode,
+                         garbage_loc)
+                       ,as.factor)) |>
+  dplyr::rename(lat_facilities = latrine_presence,
+                facilities_type = latrine_type,
+                lat_use = using_latrines,
+                nb_user = users,
+                facilities_loc = location,
+                lat_care = latrine_cleaning,
+                clean_freq_wk = cleaning_freqce_wk,
+                tk_status = tank_operation,
+                empty_freq_yr = emptying_frqce_yr,
+                empty_mode = emptying_mode,
+                empty_price = emptying_price,
+                wastwat_dispos = household_wastewater,
+                waste_cont = garbage_cont,
+                waste_cont_loc = garbage_loc,
+                cont_empty_wk = garbage_emptying_wk,
+                common_ill = common_diseases) |>
+  dplyr::mutate(nb_user = dplyr::case_when(
+    nb_user == "1_3"~"2",
+    nb_user == "4_6"~"5",
+    nb_user == "7_10"~"8.5",
+    nb_user == ">10"~ "10")) |>
+  dplyr::mutate(clean_freq_wk = dplyr::case_when(
+    clean_freq_wk == "1_3" ~ "2",
+    clean_freq_wk == ">4"~"4")) |>
+  dplyr::mutate(latrines_nb = dplyr::case_when(
+    latrines_nb == "1_5"~"3",
+    latrines_nb == "6_10"~"8",
+    latrines_nb == ">10"~"10")) |>
+  dplyr::mutate(empty_price = dplyr::case_when(
+    empty_price == "10000_20000"~"15000",
+    empty_price == "20000_40000"~"30000",
+    empty_price == ">40000"~"40000")) |>
+  dplyr::mutate(cont_empty_wk = dplyr::case_when(
+    cont_empty_wk == "1_3"~"2",
+    cont_empty_wk == "4_7"~"5.5")) |>
+  dplyr::mutate(across(c(nb_user, clean_freq_wk, latrines_nb, empty_price, cont_empty_wk), as.numeric)) |>
+  # Yes/No to logical variables
+  dplyr::mutate(across(c(lat_facilities, tank_presence, waste_cont),
+                       ~ dplyr::case_when(
+                         . == "yes" ~ TRUE,
+                         . ==  "no" ~ FALSE
+  ))) |>
+  dplyr::mutate(lat_use = dplyr::case_when(lat_use == "sharing" ~ TRUE, lat_use == "not sharing" ~ FALSE)) |>
+  dplyr::rename(lat_sharing = lat_use) |>
+  dplyr::mutate(tk_status = dplyr::case_when(tk_status == "performs well" ~ TRUE, tk_status == "not working well" ~ FALSE)) |>
 
 
 # Export Data ------------------------------------------------------------------
